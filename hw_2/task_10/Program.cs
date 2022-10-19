034575a08dbc51239106f7f795151a10b783ffa8
olebuhoe <olebuhoe@yandex.ru>
2022-10-19 14:57:09 +0300

    2-е ДЗ, 1-я задача

---
+++ hw_2/task_10/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+(Использовать только математические операции, нельзя использовать число как строку по типу number[1])
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+void SndPosition()
+{
+    Console.WriteLine("Введите трёхзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 99 && number < 1000)
+    {
+        int trdPos = number / 10;
+        int sndPos = trdPos % 10;
+        Console.WriteLine(sndPos);
+    }
+    else if (number < -99 && number > -1000)
+    {
+        int trdPos = number / 10;
+        int sndPos = -(trdPos % 10);
+        Console.WriteLine(sndPos);
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не трёхзначное число!");
+    }    
+}
+
+SndPosition();
