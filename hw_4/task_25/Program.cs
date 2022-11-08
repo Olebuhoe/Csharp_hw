d1a918a139b4aab61b407b6ef433e5d09a562555
olebuhoe <olebuhoe@yandex.ru>
2022-11-08 21:37:06 +0300

    первая задача

---
+++ hw_4/task_25/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int EnterNum(string mess)
+{
+    Console.Write(mess);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int Exponentiation(int numA, int numB)
+{
+    int numC = 1;
+
+    for (int i = 0; i < numB; i++)
+    {
+        numC *= numA;
+    }
+
+    return numC;
+}
+
+Console.WriteLine(Exponentiation(EnterNum("Введите первое число: "), EnterNum("Введите второе число: ")));
