d8cfcfabcb045c755a5af7a11b321731b7835a4c
olebuhoe <olebuhoe@yandex.ru>
2022-11-09 11:59:38 +0300

    третья задача

---
+++ hw_4/task_29/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] CreateArray(int n)
+{
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new Random().Next(1, 10);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+PrintArray(CreateArray(10));
