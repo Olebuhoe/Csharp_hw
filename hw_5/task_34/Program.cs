de1b41fcb9f3593c73a341d883998c15577bb6e5
olebuhoe <olebuhoe@yandex.ru>
2022-11-10 22:10:28 +0300

    первая задача

---
+++ hw_5/task_34/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет 
+количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] CreateArray(int n)
+{
+    int[] arr = new int[n];
+    Random rnd = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length - 1]}]");
+}
+
+int CountEvenNum(int[] array)
+{
+    int cnt = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) cnt++;        
+    }
+    return cnt;
+}
+
+int[] arr = CreateArray(7);
+PrintArray(arr);
+Console.WriteLine($"Количество четных элементов данного массива равно {CountEvenNum(arr)}.");
