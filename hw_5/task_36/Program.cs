0c605d6c352d4626b2bd381a188f1ce11f7337f3
olebuhoe <olebuhoe@yandex.ru>
2022-11-11 09:57:01 +0300

    вторая задача

---
+++ hw_5/task_36/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] CreateArray(int n)
+{
+    int[] arr = new int[n];
+    Random rnd = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rnd.Next(-99, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length - 1]}]");
+}
+
+int SumOddPos(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2) sum += array[i];
+    return sum;    
+}
+
+int[] arr = CreateArray(7);
+PrintArray(arr);
+Console.WriteLine($"Сумма элементов данного массива, стоящих на нечетных позициях равна {SumOddPos(arr)}.");
